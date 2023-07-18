7bc92cd1c3cb4684a3ed66b0c306d2c1cf04a69f
Kiran1810 <kirangill1810@gmail.com>
2023-07-18 16:15:35 +0530

    created the rooms for the group chat

---
+++ config/db-config.js
@@ -0,0 +1,10 @@
+/*const mongoose = require('mongoose');
+const{db_Url}=require('./server-config')
+
+
+const connect = async()=>{
+    await mongoose.connect(db_Url)
+}
+
+module.exports=
+    connect*/
--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+//const connect=require('./config/db-config')
 
-
+app.set('view engine','ejs')
 app.use('/',express.static(__dirname+'/public'));
 
 io.on('connection', (socket) => {
@@ -13,13 +14,32 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('user disconnected',socket.id);
       });
-      socket.on('new_msg',(data)=>{
+      //socket.on('new_msg',(data)=>{
         //io.emit('msg_rcvd',data);
-        socket.broadcast.emit('msg_rcvd',data)
+        //socket.broadcast.emit('msg_rcvd',data)
+        socket.on('join room',(data)=>{
+          console.log('join the room',data.roomId)
+          socket.join(data.roomId)
       });
+      socket.on('new_msg',(data)=>{
+        io.to(data.roomId).emit('msg_rcvd',data);
+  console.log('received data',data)
+      })
+    })
+    app.get('/chat/:roomId/:user',async(req,res)=>{
+      res.render('index',
+      {roomId:req.params.roomId,
+      user:req.params.user},
+      );
+      
+      
+    
     })
 
+    
 
-  server.listen(3000, () => {
+  server.listen(3000,//async
+  () => {
     console.log('listening on *:3000');
+     //await connect();
   }) 
